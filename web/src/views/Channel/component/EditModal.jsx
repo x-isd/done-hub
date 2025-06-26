c3660fc7648cdada066808bacd02ac97e6bf6d44
dean <1006393151@qq.com>
2025-06-26 11:26:04 +0800

    fix: Merge One Hub main branch

--- web/src/views/Channel/component/EditModal.jsx
+++ web/src/views/Channel/component/EditModal.jsx
@@ -1015,45 +1015,6 @@ const EditModal = ({ open, channelId, onCancel, onOk, groupOptions, isTag, model
                     )}
                   </FormControl>
                 )}
-
-                {inputPrompt.pre_cost && (
-                  <FormControl fullWidth error={Boolean(touched.pre_cost && errors.pre_cost)}
-                               sx={{ ...theme.typography.otherInput }}>
-                    <InputLabel htmlFor="channel-pre_cost-label">{customizeT(inputLabel.pre_cost)}</InputLabel>
-                    <Select
-                      id="channel-pre_cost-label"
-                      label={customizeT(inputLabel.pre_cost)}
-                      value={values.pre_cost}
-                      name="pre_cost"
-                      onBlur={handleBlur}
-                      onChange={handleChange}
-                      disabled={hasTag}
-                      MenuProps={{
-                        PaperProps: {
-                          style: {
-                            maxHeight: 200
-                          }
-                        }
-                      }}
-                    >
-                      {PreCostType.map((option) => {
-                        return (
-                          <MenuItem key={option.value} value={option.value}>
-                            {option.label}
-                          </MenuItem>
-                        )
-                      })}
-                    </Select>
-                    {touched.pre_cost && errors.pre_cost ? (
-                      <FormHelperText error id="helper-tex-channel-pre_cost-label">
-                        {errors.pre_cost}
-                      </FormHelperText>
-                    ) : (
-                      <FormHelperText
-                        id="helper-tex-channel-pre_cost-label"> {customizeT(inputPrompt.pre_cost)} </FormHelperText>
-                    )}
-                  </FormControl>
-                )}
                 {inputPrompt.only_chat && (
                   <FormControl fullWidth>
                     <FormControlLabel
@@ -1125,10 +1086,7 @@ const EditModal = ({ open, channelId, onCancel, onOk, groupOptions, isTag, model
                       }
                       label={customizeT(inputLabel.compatible_response)}
                     />
-                    <FormHelperText id="helper-tex-compatible_response-label">
-                      {' '}
-                      {customizeT(inputPrompt.compatible_response)}{' '}
-                    </FormHelperText>
+                    <FormHelperText id="helper-tex-compatible_response-label">{customizeT(inputPrompt.compatible_response)}</FormHelperText>
                   </FormControl>
                 )}
                 {pluginList[values.type] &&
